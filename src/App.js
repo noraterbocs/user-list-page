782a8a6fd3c2f7b050de4e1530b59b000b8db721
Nora Terbocs <nora.terbocs@gmail.com>
2022-09-25 12:35:18 +0200

    Added previous button

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const url = `https://dummyjson.com/users?limit=20&skip=${count}`;
+  const setCountToPrevious = () => {
+    if (count !== 0) {
+      setCount(count - 20);
+    } else {
+      console.log("zero");
+    }
+  };
   return (
     <div className="App">
       <Users className="item" url={url} changePage={count} />
+      <button className="btn" onClick={() => setCountToPrevious()}>
+        Previous
+      </button>
       <button className="btn" onClick={() => setCount(count + 20)}>
-        Next page
+        Next
       </button>
     </div>
   );
